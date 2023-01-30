be703c56203e23a632b8874e1690c1df74472ba6
rmaniva <rmaniva@NLTI159>
2023-01-30 17:54:55 +0530

    Add project files.

---
+++ program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+       
+        int num1, num2, temp;
+        //ask for numbers
+        Console.WriteLine("\nEnter the First Number : ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nEnter the Second Number : ");
+        num2 = int.Parse(Console.ReadLine());
+      
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+      
+        Console.WriteLine("After Swapping : ");
+        Console.WriteLine("First Number : " + num1);
+        Console.WriteLine("Second Number : " + num2);
+
+    }
+}
